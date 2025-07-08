260b1f92ccb1d8708b8653588e7792be79ae3b51
MeshachB <MeshachCBrown@gmail.com>
2025-07-07 21:31:45 -0400

    Added initial JS, CSS, and duo data (added) object array

---
+++ data.js
@@ -0,0 +1,30 @@
+
+const dousCards = [
+{ match:"Kobe", made:"Shaq" },
+{ match:"Batman", made:"Robin"},
+{ match:"Raekwon",made:"Gostface"},
+{ match:"Jordan", made:"Pippen"},
+{ match:"Will", made:"Carlton"},
+{ match:"Mario",made:"Luigi"},
+{ match:"Woddy",made:"Buzz"},
+{ match:"SpongeBob", made:"Patrick"},
+{ match:"Lilo", made:"Stitch"},
+{ match:"Maverick", made:"Goose"},
+{ match:"Matt Damon", made:"Ben Affleck"},
+{ match:"Denzel", made: "Spike"},
+{ match:"Andre 3000", made:"Big Boi"},
+{ match:"Naruto", made:"Sasuke"},
+{ match:"Goku", made:"Vegeta"},
+{ match:"Tom", made:"Jerry"},
+{ match:"Starsky", made:"Hutch"},
+{ match:"Snoop", made:"DR. DRE"},
+{ match: "Erick B", made:"Rakim"},
+{ match:"LeBron", made:"D-Wade"},
+{ match:"Luffy", made:"Zoro"},
+{ match: "Steph", made:"Klay"},
+{ match: "Redman", made: "Method Man"},
+{ match:"Kid", made:"Play"},
+{match: "Jay-z", made:"Beyoncé"}
+]
+
+
